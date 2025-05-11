139905b26bd4c13e1b5f6a51b3c0106bde41fbf9
Simranjit Singh <sisingh@husky.ca>
2025-05-11 18:00:49 -0400

    Improved CSS. Logout and Change password pages.

--- battery_tracker/battery_manager/urls.py
+++ battery_tracker/battery_manager/urls.py
@@ -1,8 +1,12 @@
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
+from django.urls import path
 
 urlpatterns = [
     # ... your existing urls ...
+    path('accounts/password_change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
+    path('accounts/password_change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
 ]
 
 if settings.DEBUG:
